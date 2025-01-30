44b4d053e0d1ca45068af1b0c25b606e884d2002
Yan Goncalves <yangoncalves@develowl.com.br>
2025-01-30 19:22:17 -0300

    feat(typescript-challenge): add types and generics challenge

---
+++ challenges/typescript/types-generics/index.ts
@@ -0,0 +1,14 @@
+export function extractValues<
+  T extends Record<keyof T, any>,
+  K extends keyof T
+>(data: T[], field: K) {
+  if (!(Array.isArray(data) && data.every((obj) => typeof obj === 'object'))) {
+    throw new Error('Provided data should be an array of objects');
+  }
+
+  if (!data.every((obj) => field in obj)) {
+    throw new Error(`Provided field is not a key of data`);
+  }
+
+  return data.map((obj) => obj[field]);
+}
