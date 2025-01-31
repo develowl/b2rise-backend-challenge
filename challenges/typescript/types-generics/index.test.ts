e579f965ef78d4430b05be803e078dab57be6d0e
Yan Goncalves <yangoncalves@develowl.com.br>
2025-01-30 21:24:19 -0300

    refactor(typescript-challenge): implement tests usint vitest

--- challenges/typescript/types-generics/index.test.ts
+++ challenges/typescript/types-generics/index.test.ts
@@ -1,5 +1,4 @@
-import assert from 'node:assert';
-import { describe, it } from 'node:test';
+import { describe, expect, it } from 'vitest';
 import { extractValues } from '.';
 
 describe('Types and Generics manipulation', () => {
@@ -11,23 +10,23 @@ describe('Types and Generics manipulation', () => {
   it('should throw an error when data is not an array of objects', () => {
     const modifiedData = [...mockData, 1];
 
-    assert.throws(() => {
-      extractValues<any, any>(modifiedData, 'field');
-    });
+    expect(() =>
+      extractValues(modifiedData, 'id' as keyof object)
+    ).toThrowError(new Error('Provided data should be an array of objects'));
   });
 
   it('should throw an error when field is not a key of object from data', () => {
     const modifiedData = [...mockData, { id: 3, age: 28 }];
 
-    assert.throws(() => {
-      extractValues(modifiedData, 'name' as any);
-    });
+    expect(() =>
+      extractValues(modifiedData, 'age' as keyof object)
+    ).toThrowError(new Error(`Provided field is not a key of data`));
   });
 
   it('should return values related to provided field', () => {
     const result = extractValues(mockData, 'name');
     const values = ['Alice', 'Bob'];
 
-    assert.deepEqual(result, values);
+    expect(result).toEqual(values);
   });
 });
