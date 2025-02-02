69cc9755e6dd2f29384f8eb9192e765e8d6664bb
Yan Goncalves <yangoncalves@develowl.com.br>
2025-02-02 19:01:25 -0300

    test(clean-code-challenge): add complex logic refactoring challenge

---
+++ challenges/3_clean-code/3.2_complex-logic-refactoring/index.test.ts
@@ -0,0 +1,28 @@
+import { describe, expect, it } from 'vitest';
+import { calculateDiscount } from '.';
+
+describe('Complex logic refactoring', () => {
+  it('should return correct value when is premium and the price is greatest than hundred', () => {
+    const result = calculateDiscount(120, true);
+
+    expect(result).toBe(120 * 0.8);
+  });
+
+  it('should return correct value when is premium and the price is less or equal hundred', () => {
+    const result = calculateDiscount(100, true);
+
+    expect(result).toBe(100 * 0.9);
+  });
+
+  it('should return correct value when is not premium and the price is greatest than hundred', () => {
+    const result = calculateDiscount(126, false);
+
+    expect(result).toBe(126 * 0.9);
+  });
+
+  it('should return correct value when is not premium and the price is less or equal hundred', () => {
+    const result = calculateDiscount(100, false);
+
+    expect(result).toBe(100);
+  });
+});
